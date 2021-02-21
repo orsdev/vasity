e307ed89412db3d849fa9585cd2cf08ceab2bb95
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2021-02-21 08:17:18 +0100

    mobile gridTemplateRows defined in App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import RootThemeProvider from './components/ChakraProvider';
 import Header from './components/Header';
+import Testimonial from './pages/Testimonial';
 
 function App() {
   return (
@@ -14,8 +15,12 @@ function App() {
               '[gap-start] 10rem [gap-end full-start] minmax(6rem, 1fr) [center-start] repeat(8, [col-start] minmax(min-content, 14rem) [col-end]) [center-end] minmax(6rem, 1fr) [full-end]'
           }}
           gridTemplateRows={{
-            base: '80vh repeat(4, 40vw) min-content'
-          }}></Box>
+            base:
+              'Repeat(2, min-content) repeat(4, 40vw) min-content',
+            md: '80vh repeat(4, 40vw) min-content'
+          }}>
+          <Testimonial />
+        </Box>
       </RootThemeProvider>
     </>
   );
