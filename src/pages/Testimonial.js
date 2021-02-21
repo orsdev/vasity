d49c4db415c65f67346a0c6b614c092ebfbf2596
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2021-02-21 13:25:40 +0100

    Experience component used in Testimonials component

--- src/pages/Testimonial.js
+++ src/pages/Testimonial.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Experience, {
+  experiences
+} from '../components/Experiences/Experience';
+import ToluxJoy from '../components/Experiences/ToluxJoy';
 
 function Testimonial() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Experience>
+        <ToluxJoy name={experiences['toluxjoy']} />
+      </Experience>
+    </>
+  );
 }
 
 export default Testimonial;
